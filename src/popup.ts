3c97fce0d0444ffe28738965f53756fe12bfc678
Łukasz Boczoń <lukasz.boczon@gotoma.pl>
2021-04-04 10:54:16 +0200

    add button

--- src/popup.ts
+++ src/popup.ts
@@ -3,6 +3,9 @@ import "./style/style.css";
 let disableTranslation = document.getElementById(
     "disableTranslation"
 ) as HTMLButtonElement;
+let addTranslation = document.getElementById(
+    "addTranslate"
+) as HTMLButtonElement;
 let buttonTranslate = document.getElementById(
     "buttonTranslate"
 ) as HTMLButtonElement;
@@ -22,6 +25,18 @@ disableTranslation.addEventListener("click", async () => {
     });
 
     disableTranslation.style.backgroundColor = "green";
+});
+
+addTranslation.addEventListener("click", () => {
+    chrome.storage.sync.get("translate", (data) => {
+        let translate = data.translate;
+
+        chrome.storage.sync.set({
+            translate: translate.concat(`,${inputTranslate.value}`),
+        });
+
+        setTextTranslate();
+    });
 });
 
 buttonTranslate.addEventListener("click", () => {
@@ -48,7 +63,7 @@ function setTextTranslate() {
 }
 
 function init() {
-    chrome.storage.sync.set({ translate: ".token-line, pre, h1, a" });
+    chrome.storage.sync.set({ translate: ".token-line,pre,h1,a" });
     setTextTranslate();
 }
 
